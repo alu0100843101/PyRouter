497983a8f8e30e722bfda8afd9180aadb232f5d6
alu0100843101 <alu0100843101@ull.edu.es>
2018-05-09 17:07:17 +0100

    ya
    ya

--- mytopo.py
+++ mytopo.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# -*- coding: utf-8 -*-
 '''
 ################################################################################
 ##### PROYECTO FINAL DE LABORATORIO DE REDES EN INGENIERÍA DE COMPUTADORES #####
